502c0c476c84fb507d0ea71b67734b5509a45e34
MnichPawel <pawel.aleksander.mnich@gmail.com>
2021-12-02 21:17:26 +0100

    added menu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        while(true) {
+            System.out.println("*=================== Menu ===================*");
+            System.out.println("|1 - Przewidywanie rodzaju odpowiedzi        |");
+            System.out.println("|2 - Wyjdz                                   |");
+            System.out.println("|============================================|");
+
+            Scanner scanner = new Scanner(System.in);
+            int menu_val = scanner.nextInt();
+
+            if (menu_val == 1){
+                System.out.println("Hello world");
+            } else if(menu_val == 2){
+                break;
+            }
+        }
     }
 }
